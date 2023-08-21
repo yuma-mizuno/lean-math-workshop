390c3d694e5e5017e242d73bee9cae326fe67abd
Yuma Mizuno <mizuno.y.aj@gmail.com>
2023-08-21 14:40:53 +0900

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -1,19 +1,20 @@
 import Lake
 open Lake DSL
 
-def moreLeanArgs := #[
-  "-Dpp.unicode.fun=true" -- pretty-prints `fun a ↦ b`
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-DrelaxedAutoImplicit=false"
 ]
 
 package «lean-math-workshop» {
   -- add any package configuration options here
   -- moreLeanArgs := moreLeanArgs
-  moreServerArgs := moreLeanArgs
+  moreServerArgs := moreServerArgs
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-    @"08fc07cd8ba56396909a84a7b2361cfda8223840"
+    @"c6e395dd3bb117a6db28f2b094bc822994f7cee9"
 
 @[default_target]
 lean_lib «Tutorial» {
