5a01f660f2fb67ad3d374c22cc304d1ed15bd7ac
Yuma Mizuno <mizuno.y.aj@gmail.com>
2023-08-21 12:21:41 +0900

    Update Tutorial/Advanced/Category/Lecture1.lean
    Co-authored-by: Haruhisa Enomoto <73296681+haruhisa-enomoto@users.noreply.github.com>

--- Tutorial/Advanced/Category/Lecture1.lean
+++ Tutorial/Advanced/Category/Lecture1.lean
@@ -170,6 +170,9 @@ instance {A B : CommAlgCat R} : AlgHomClass (Hom A B) R A B :=
 /- 少し毛色の違う例として、対象が何もない圏を構成しよう。
 これは対象の集合が空集合`Empty`であるような圏である。 -/
 -- ヒント: 空集合からの空写像は`Empty.elim`で表される
-instance : Category Empty := sorry
+instance : Category Empty where
+  Hom a b := sorry
+  comp f g := sorry
+  id a := sorry
 
 end Tutorial
