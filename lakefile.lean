45efc07d9db1206c1ea607de3201ac9b30b13188
Seasawher <tellagoodstory@icloud.com>
2024-04-10 00:18:57 +0900

    update lean and mathlib version to v4.7.0

--- lakefile.lean
+++ lakefile.lean
@@ -14,7 +14,7 @@ require «mk-exercise» from git
   "https://github.com/Seasawher/mk-exercise.git" @ "70102f2963f5bc19aac0339d9464b72a1de2e1cb"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "7ca43cbd6aa34058a1afad8e47190af3ec1f9bdb"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.7.0"
 
 @[default_target]
 lean_lib «Solution» where
