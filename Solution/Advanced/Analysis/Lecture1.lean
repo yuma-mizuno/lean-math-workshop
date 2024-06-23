1403bbca7860ac3878976dbbbb2e38b55f69d592
Seasawher <tellagoodstory@icloud.com>
2024-06-24 01:51:17 +0900

    実情を反映しないヒントを削除する

--- Solution/Advanced/Analysis/Lecture1.lean
+++ Solution/Advanced/Analysis/Lecture1.lean
@@ -325,8 +325,6 @@ theorem HasDerivAt.mul {f : ℝ → ℝ} (hf : HasDerivAt f f' a) (hg : HasDeriv
       ring
       -- sorry
 
--- 次の問題で使うかも？
-#check Nat.succ_eq_add_one
 
 /-- 単項式の微分 -/
 theorem hasDerivAt_pow (n : ℕ) (a : ℝ) :
